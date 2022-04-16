ac07f5614679bdf49dd7e1f5c207c63f3fea1dff
Kenton Wang <smartepsh@gmail.com>
2022-04-16 23:12:13 +0800

    chore: prepare for publish

--- mix.exs
+++ mix.exs
@@ -1,13 +1,20 @@
 defmodule SCF.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :tencent_cloud_scf,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      package: package(),
+      description: description(),
+      name: "TencentCloudSCF",
+      source_url: github_url()
     ]
   end
 
@@ -23,7 +30,30 @@ defmodule SCF.MixProject do
     [
       {:tesla, "~> 1.4"},
       {:hackney, "~> 1.17"},
-      {:jason, "~> 1.2"}
+      {:jason, "~> 1.2"},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
     ]
+  end
+
+  defp docs do
+    [
+      main: "readme",
+      source_url: github_url(),
+      source_ref: "v#{@version}",
+      extras: ["README.md", "LICENSE"]
+    ]
+  end
+
+  defp description, do: "The Tencent Cloud SCF SDK Elixir Version."
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"Github" => github_url()}
+    ]
+  end
+
+  defp github_url do
+    "https://github.com/smartepsh/tencent_cloud_scf.git"
   end
 end
